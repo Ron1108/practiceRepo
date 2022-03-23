98d5120ec82d464f8c43ca37b2e835f5b5e5533e
Chakraborty <ronit.chakraborty@atos.net>
2022-03-23 17:05:24 +0530

    commit for push request on master

--- update_main_branch.sh
+++ update_main_branch.sh
@@ -10,12 +10,14 @@ then
 #if user wants to execute commit and push to master
 elif [ $1 == "push" ] && [ $2 == "master" ]
 then
-	git checkout master
+	#git checkout master
 	echo "in master"
         git add .
         git commit -m "commit for push request on master"
-        #git checkout master
+        git checkout master
 	#git pull origin master
+	git add .
+        git commit -m "commit for push request on master"
 	git push origin master
 #if user wants to execute commit and push to dev
 elif [ $1 == "push" ]
