9e9b80b6e6d352205a89258632912e39ab7ef5bf
Chakraborty <ronit.chakraborty@atos.net>
2022-03-23 11:52:42 +0530

    commit for push request on master

--- update_main_branch.sh
+++ update_main_branch.sh
@@ -2,12 +2,24 @@
 #!/bin/bash
 if [ $1 == "commit" ]
 then
+	#adding files to the staging are
+	git add .
+	#commiting current snapshot to the local repo
+	git commit -m "commit changes to local repo"
 
-#add changes to the staging area
-git add .
 
-#commit the current snapshot to the local branch 
-git commit -m "script test commit"
-
+#if user wants to execute commit and push to master
+elif [ $1 == "push" ]
+then
+	git add .
+	git commit -m "commit for push request on master"
+	git push origin master
+#if user wants to run the project and then push to master 
+elif [ $1 == "run " ]
+then
+	mvn clean test -p $2
+	git add .
+	git commit -m "commiting after execution of project"
+	git push origin master
 fi
 
