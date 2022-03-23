12bd71af1d318fa0dcec87602b74d4eb750e4d31
Chakraborty <ronit.chakraborty@atos.net>
2022-03-23 10:33:21 +0530

    script test commit

--- update_main_branch.sh
+++ update_main_branch.sh
@@ -1,2 +1,13 @@
+
+#!/bin/bash
+if [ $1 == "commit" ]
+then
+
+#add changes to the staging area
 git add .
+
+#commit the current snapshot to the local branch 
 git commit -m "script test commit"
+
+fi
+
