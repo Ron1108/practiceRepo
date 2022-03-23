274807e555dbbb9481f1e39daea94190cd899ed2
Chakraborty <ronit.chakraborty@atos.net>
2022-03-23 12:19:41 +0530

    commiting after execution of project

--- update_main_branch.sh
+++ update_main_branch.sh
@@ -15,11 +15,13 @@ then
 	git commit -m "commit for push request on master"
 	git push origin dev
 #if user wants to run the project and then push to master 
-elif [ $1 == "run " ]
+elif [ $1 == "run" ]
 then
-	mvn clean test -p $2
+	mvn clean install -P $2
 	git add .
 	git commit -m "commiting after execution of project"
 	git push origin master
+else
+	echo "incorrect input"
 fi
 
