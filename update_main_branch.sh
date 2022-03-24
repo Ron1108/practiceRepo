90b01bd545c2717ecdb4bff46c3f1a618d0f2c45
Chakraborty <ronit.chakraborty@atos.net>
2022-03-24 14:51:50 +0530

    commit for push request on master

--- update_main_branch.sh
+++ update_main_branch.sh
@@ -7,12 +7,18 @@ then
 	#commiting current snapshot to the local repo
 	git commit -m "commit changes to local repo"
 
+elif [ $1 == "push" ] && [ $2 = "master" ]
+then
+        git add .
+        git commit -m "commit for push request on master"
+        git checkout master
+	git push origin master
 
 #if user wants to execute commit and push to master
 elif [ $1 == "push" ]
 then
 	git add .
-	git commit -m "commit for push request on master"
+	git commit -m "commit for push request on dev"
 	git push origin dev
 #if user wants to run the project and then push to master 
 elif [ $1 == "run" ]
