568b22ab9a713eb32e4894b4478638c3f87cb424
Chakraborty <ronit.chakraborty@atos.net>
2022-04-04 12:27:22 +0530

    commit for push request on dev

--- src/test/java/basic/file/test/BasicFileTests.java
+++ src/test/java/basic/file/test/BasicFileTests.java
@@ -1,27 +1,30 @@
 package basic.file.test;
 
-import static org.junit.Assert.assertEquals;
-
+//import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.junit.After;
-//import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
+//import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import edu.file.operations.FileOperations;
+import edu.file.operations.*;
 
-public class BasicFileTests {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class BasicFileTest {
 	FileOperations fileObject = new FileOperations();
 	FileReader fileReaderForTest;
 	File testFile;
 	Properties fileProp;
 	String sampleText, checkText;
 
-	@Before
+	@BeforeEach
 	public void initFile() throws IOException {
 		//fileReaderForTest = getClass().getClassLoader().getResourceAsStream(("user.dir") + "src\\test\\resource\\path.properties");
 		fileReaderForTest = new FileReader(System.getProperty("user.dir") + "\\src\\test\\resources\\path.properties");
@@ -33,15 +36,16 @@ public class BasicFileTests {
 		checkText = fileProp.getProperty("check");
 	}
 
-//	@After
-//	public void destroyFile()
-//	{
+	@AfterAll
+	public void destroyFile()
+	{
 //		if(testFile.exists())
 //			testFile.delete();
-//	}
+	}
 	@Test
+	@Order(1)
 	public void checkCreateNewFile() {
-		// destroyFile();
+		destroyFile();
 		fileObject.createNewFile();
 		File testFile = new File(fileProp.getProperty("path"));
 		assertEquals(true, testFile.exists());
@@ -49,6 +53,7 @@ public class BasicFileTests {
 	}
 
 	@Test
+	@Order(2)
 	public void checkWriteFile() throws IOException {
 		fileObject.writeToFile(sampleText);
 		System.out.println("Test 2 ok");
@@ -56,14 +61,16 @@ public class BasicFileTests {
 	}
 
 	@Test
+	@Order(3)
 	public void checkReadFile() {
 		assertEquals(checkText, fileObject.readFromFile());
 		System.out.println("Test 3 ok");
 	}
-//	@Test
-//	public void checkDelete()
-//	{
-//		fileObject.deleteFile();
-//		assertEquals(false,testFile.exists());
-//	}
+	@Test
+	@Order(6)
+	public void checkDelete()
+	{
+		fileObject.deleteFile();
+		assertEquals(false,testFile.exists());
+	}
 }
