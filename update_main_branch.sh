397a42a36bcc4411053a3fac004590162220abc8
Chakraborty <ronit.chakraborty@atos.net>
2022-04-04 12:44:24 +0530

    commit changes to local repo

--- update_main_branch.sh
+++ update_main_branch.sh
@@ -4,13 +4,13 @@ then
 	#Asking for the file name as user input 
 	echo "Enter the file name to be commited :"
 	read fileName
-		if [$fileName == "all" ]
-		then
-			#adding files to the staging are
-			git add .
-		else
-			git add $fileName
-		fi
+	if [ $fileName == "all" ]
+	then
+		#adding files to the staging are
+		git add .
+	else
+		git add $fileName
+	fi
 	#commiting current snapshot to the local repo
 	git commit -m "commit changes to local repo"
 
