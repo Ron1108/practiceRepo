42efc986738d2e61d43f85e45f8033810488dbc3
Chakraborty <ronit.chakraborty@atos.net>
2022-04-04 14:07:49 +0530

    commit for push request on dev

---
+++ update_git_branch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "Enter the file name to be commited :"
+read task
+if [ $task == "commit" ]
+then
+	#Asking for the file name as user input 
+	echo "Enter the file name to be commited :"
+	read fileName
+	if [ $fileName == "all" ]
+	then
+		#adding files to the staging are
+		git add .
+	else
+		git add $fileName
+	fi
+	#commiting current snapshot to the local repo
+	git commit -m "commit changes to local repo"
+
+#if user wants to push to master
+elif [ $1 == "push" ] && [ $2 = "master" ]
+then
+        git add .
+        git commit -m "commit for push request on master"
+        git checkout master
+	#git pull origin master
+	git add .
+  git commit -m "commit for push request on master"
+	git pull origin master
+	git push origin master
+	git checkout dev
+
+#if user wants to execute commit and push to dev
+elif [ $task == "push" ]
+then
+	git add .
+	git commit -m "commit for push request on dev"
+	git push origin dev
+#if user wants to run the project and then push to master 
+elif [ $task == "run" ]
+then
+	mvn clean install -P $2
+	git add .
+	git commit -m "commiting after execution of project"
+	git push origin dev
+else
+	echo "incorrect input"
+fi
+
